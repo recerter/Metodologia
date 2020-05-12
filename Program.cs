44135d19c71434b1de9fca6f1b4396b8dcc6821a
Leandro <leandro.oviedo96@gmail.com>
2020-05-12 11:12:55 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Metodologia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
